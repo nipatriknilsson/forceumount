d36ac9a7939ed57cbc0b0c9e306da2abc0e2f60f
Patrik Nilsson <nipatriknilsson@gmail.com>
2022-04-17 09:55:51 +0200

    better help, more device support

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ All processes and threads holding devices are killed. First gently by SIGTERM an
 
 usage: forceumount.sh {one_or_more_devices}
 
+The device name can be a block device (i.e. /dev/sdc), a loop back device (i.e. /dev/loop0) or name of an iso-file (i.e. /home/user/Downloads/ubuntu.iso).
+
 Tested with Ubuntu.
--- forceumount.sh
+++ forceumount.sh
@@ -5,7 +5,9 @@
 help=0
 declare -a blkdevices
 
-while [ $# -gt 0 ]; do
+[ $# == 0 ] && help=1
+
+while [ $# -gt 0 ] ; do
     case "$1" in
         --help)
             help=1
@@ -25,9 +27,16 @@ while [ $# -gt 0 ]; do
 done
 
 if [ "$help" == "1" ] ; then
-    echo "$0 {blockdevice} [blockdevice]..."
-    echo "Try to kill processes holding a block device and then deactivate the device."
-    echo "Nonexisting blockdevices are ignored"
+    echo "$0 {one_or_more_devices}"
+    echo
+    echo "\
+Try to kill processes and threads holding a device and then \
+deactivate it."
+    echo "Non existing devices are ignored."
+    echo "\
+The device name can be a block device (i.e. /dev/sdc), a loop back \
+device (i.e. /dev/loop0) or name of an iso-file \
+(i.e. /home/user/Downloads/ubuntu.iso)."
     echo
     echo "Copyright 2022 Patrik Nilsson. MIT License."
     exit 1
@@ -43,11 +52,11 @@ sendkill()
     dev=$1
     sig=$2
     
-    lsblk -r -n -o MOUNTPOINT "${dev}" |
+    lsblk -n -o MOUNTPOINT "${dev}" |
     while IFS= read -r f ; do
         if [ "$f" != "" ] ; then
             for (( i=0 ; i<30 ; i++ )) ; do 
-                fuser -m "$f" 2>/dev/null | awk '{print $0}' | tr -d ' ' | grep -v -E '^$' |
+                fuser -m "$f" 2>/dev/null | awk '{print $0}' | tr ' ' '\n' | grep -v -E '^$' |
                 while IFS= read -r g ; do
                     if [ "$g" != "" ] ; then
                         kill -$sig -- "-$g" || true
@@ -56,7 +65,7 @@ sendkill()
                 echo -n "."
                 
                 sleep 1s
-                moretokill=$(lsblk -r -n -o MOUNTPOINT "${dev}" | xargs -r -I '{}' -- fuser '{}' 2>/dev/null | tr ' ' '\n' | grep -v '^$' | wc -l)
+                moretokill=$(lsblk -n -o MOUNTPOINT "${dev}" | xargs -r -I '{}' -- fuser '{}' 2>/dev/null | tr ' ' '\n' | grep -v '^$' | wc -l)
                 if [ "$moretokill" == "0" ] ; then
                     break
                 fi
@@ -81,8 +90,16 @@ for blkdevice in "${blkdevices[@]}" ; do
         sendkill "${umountdev}" KILL
         echo " Done!"
         
-        lsblk -r -n -o MOUNTPOINT "${umountdev}" | while IFS= read -r f ; do if [ "$f" != "" ] ; then swapon --show=NAME --noheadings | grep -E "^$f" | xargs -I '{}' -- swapoff '{}' ; fi ; done
+        # swap
+        lsblk -n -o MOUNTPOINT "${umountdev}" | while IFS= read -r f ; do if [ "$f" != "" ] ; then swapon --show=NAME --noheadings | grep -E "^$f" | xargs -I '{}' -- swapoff '{}' ; fi ; done
         
+        # meant for loop devices
+        lsblk -n -p -l "${umountdev}" | tac | awk '{print $1}' | xargs -r -I '{}' -- umount '{}' 2>/dev/null
+        
+        # deactivate loop devices
+        lsblk -n -p -l "${umountdev}" | tac | awk '{print $1}' | xargs -r -I '{}' -- losetup -d '{}' 2>/dev/null
+        
+        # the rest
         lsblk -n -p -l "${umountdev}" | tac | awk '{print $1}' | xargs -r -I '{}' -- blkdeactivate -d force,retry -u -l wholevg -m disablequeueing -r wait '{}'
     fi
 done
